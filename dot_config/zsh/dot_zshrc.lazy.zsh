efe9d72c66b412b5137b8f900901b41858185fd2
Ciffelia <mc.prince.0203@gmail.com>
2022-10-25 00:36:12 +0900

    Update

--- dot_config/zsh/dot_zshrc.lazy.zsh
+++ dot_config/zsh/dot_zshrc.lazy.zsh
@@ -1,5 +1,11 @@
+# --option=abc みたいなやつでも補完
+setopt MAGIC_EQUAL_SUBST
+
 # 補完をTab連打ではなく矢印キーで選択する
 zstyle ':completion:*' menu select interactive
+
+# Alias
+alias reload='exec $SHELL --login'
 
 # Plugins
 zinit lucid blockf light-mode for \
@@ -12,6 +18,3 @@ zinit ice lucid \
     as'program' from'gh-r' \
     atload'eval "$(mcfly init zsh)"'
 zinit light cantino/mcfly
-
-# Alias
-alias reload='exec $SHELL --login'
