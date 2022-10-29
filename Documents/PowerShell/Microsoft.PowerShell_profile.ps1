c3d142049b9e55647bce9838619892342ea55f9d
Ciffelia <mc.prince.0203@gmail.com>
2022-10-29 17:13:56 +0900

    Add `ls`, `la`, `ll` aliases for PowerShell

--- Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -16,6 +16,14 @@ Set-Alias -Name cd -Value pushd -Option AllScope
 # Use curl from scoop, instead of one in C:\Windows\System32
 Set-Alias curl ~\scoop\shims\curl.exe
 
+Set-Alias ls lsd
+Function la() {
+  lsd -a $args
+}
+Function ll() {
+  lsd -la $args
+}
+
 # Chocolatey
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
