2517a104122826c01e8e693c7ad484bd8fb1d659
Ciffelia <mc.prince.0203@gmail.com>
2022-10-29 16:34:02 +0900

    Add PowerShell profile

---
+++ Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,33 @@
+﻿# Source: https://stackoverflow.com/a/29424207
+Function Test-CommandExists($name) {
+  [bool](Get-Command -Name $name -ErrorAction SilentlyContinue)
+}
+
+Function which($name) {
+  $command = Get-Command -Name $name -ErrorAction SilentlyContinue
+  if ($command) {
+    $command.Definition
+  }
+}
+
+# cd -> pushd
+Set-Alias -Name cd -Value pushd -Option AllScope
+
+# Use curl from scoop, instead of one in C:\Windows\System32
+Set-Alias curl ~\scoop\shims\curl.exe
+
+# Chocolatey
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+# Volta
+if (Test-CommandExists("volta")) {
+  (& volta completions powershell) | Out-String | Invoke-Expression
+}
+
+# Starship
+if (Test-CommandExists("starship")) {
+  Invoke-Expression (&starship init powershell)
+}
