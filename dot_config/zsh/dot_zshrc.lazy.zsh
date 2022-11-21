8e18a31319484ce15af6b308d99bee9dbd507f36
Ciffelia <mc.prince.0203@gmail.com>
2022-11-22 00:57:29 +0900

    Load zsh-completions before zsh-autosuggestions see https://github.com/sorin-ionescu/prezto/tree/master/modules/syntax-highlighting

--- dot_config/zsh/dot_zshrc.lazy.zsh
+++ dot_config/zsh/dot_zshrc.lazy.zsh
@@ -61,12 +61,12 @@ zinit light twpayne/chezmoi
 zinit ice lucid blockf
 zinit light zdharma-continuum/fast-syntax-highlighting
 
-# autosuggestions
-zinit ice lucid blockf \
-  atload'!_zsh_autosuggest_start'
-zinit light zsh-users/zsh-autosuggestions
-
 # completions
 zinit ice lucid blockf \
   atload'zicompinit; zicdreplay'
 zinit light zsh-users/zsh-completions
+
+# autosuggestions
+zinit ice lucid blockf \
+  atload'!_zsh_autosuggest_start'
+zinit light zsh-users/zsh-autosuggestions
