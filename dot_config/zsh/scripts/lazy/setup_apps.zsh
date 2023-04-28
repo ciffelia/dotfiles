de3bf6b2baa70e088d935cbf415265b8221dceba
Ciffelia <mc.prince.0203@gmail.com>
2023-04-29 03:23:32 +0900

    Move pyenv initialization to `.zshenv`

--- dot_config/zsh/scripts/lazy/setup_apps.zsh
+++ dot_config/zsh/scripts/lazy/setup_apps.zsh
@@ -1,10 +1,3 @@
-# pyenv
-if [ -d "$HOME/.pyenv" ]; then
-  export PYENV_ROOT="$HOME/.pyenv"
-  command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
-  eval "$(pyenv init -)"
-fi
-
 # kubectl
 if (( $+commands[kubectl] )); then
   source <(kubectl completion zsh)
