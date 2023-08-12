890a47e7e857de1233798a51b6152ca3ba9b8d94
Ciffelia <mc.prince.0203@gmail.com>
2023-08-12 20:33:51 +0900

    Update `change-git-directory-with-incremental-search`

--- dot_config/zsh/scripts/lazy/functions.zsh
+++ dot_config/zsh/scripts/lazy/functions.zsh
@@ -1,9 +1,10 @@
 # ghqで管理しているリポジトリをインクリメンタルサーチし、Enterキーを押下した際に該当フォルダに移動
 # cf. https://tech-broccoli.life/articles/engineer/use-fzf
 function change-git-directory-with-incremental-search () {
-  local WORKDIR=$(ghq list -p | fzf --preview "onefetch --no-art {}" --height 70%)
-  [ -z "$WORKDIR" ] && return
-  cd $WORKDIR
+  local ghq_root=$(ghq root)
+  local repo=$(ghq list | fzf --preview "onefetch --no-art '$ghq_root/{}'" --height 70%)
+  [ -z "$repo" ] && return
+  cd "$ghq_root/$repo"
 }
 
 alias g='change-git-directory-with-incremental-search'
