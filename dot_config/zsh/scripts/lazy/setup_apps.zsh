4955ae7b4b3a8cb6d98ca944f55f327775b93b04
Ciffelia <mc.prince.0203@gmail.com>
2024-01-20 18:17:51 +0900

    refactor

--- dot_config/zsh/scripts/lazy/setup_apps.zsh
+++ dot_config/zsh/scripts/lazy/setup_apps.zsh
@@ -1,12 +1,37 @@
+# Helm
+if (( $+commands[helm] )); then
+  source <(helm completion zsh)
+fi
+
 # kubectl
 if (( $+commands[kubectl] )); then
   source <(kubectl completion zsh)
 fi
 
-# Helm
-if (( $+commands[helm] )); then
-  source <(helm completion zsh)
-fi
+# bat
+zinit ice lucid \
+  as'program' from'gh-r' \
+  pick'bat-*/bat' \
+  cp'bat-*/autocomplete/bat.zsh -> _bat'
+zinit light sharkdp/bat
+
+# chezmoi
+zinit ice lucid \
+  as'program' from'gh-r' \
+  bpick'chezmoi_*.tar.gz' \
+  cp'completions/chezmoi.zsh -> _chezmoi'
+zinit light twpayne/chezmoi
+
+# delta
+zinit ice lucid \
+  as'program' from'gh-r' \
+  pick'delta-*/delta'
+zinit light dandavison/delta
+
+# difftastic
+zinit ice lucid \
+  as'program' from'gh-r'
+zinit light Wilfred/difftastic
 
 # fzf
 zinit ice lucid \
@@ -28,19 +53,11 @@ export FZF_DEFAULT_OPTS="--height 50% --layout=reverse --border \
   --bind 'ctrl-/:change-preview-window(80%|hidden|)' \
   --bind 'shift-up:preview-half-page-up,shift-down:preview-half-page-down'"
 
-# onefetch
+# ghq
 zinit ice lucid \
   as'program' from'gh-r' \
-  atclone'./onefetch --generate zsh > _onefetch' \
-  atpull"%atclone"
-zinit light o2sh/onefetch
-
-# mcfly
-zinit ice lucid \
-  as'program' from'gh-r' \
-  bpick'*-x86_64-*' \
-  atload'eval "$(mcfly init zsh)"'
-zinit light cantino/mcfly
+  pick'ghq_*/ghq'
+zinit light x-motemen/ghq
 
 # lsd
 zinit ice lucid \
@@ -48,30 +65,12 @@ zinit ice lucid \
   pick'lsd-*/lsd'
 zinit light lsd-rs/lsd
 
-# bat
+# mcfly
 zinit ice lucid \
   as'program' from'gh-r' \
-  pick'bat-*/bat' \
-  cp'bat-*/autocomplete/bat.zsh -> _bat'
-zinit light sharkdp/bat
-
-# delta
-zinit ice lucid \
-  as'program' from'gh-r' \
-  pick'delta-*/delta'
-zinit light dandavison/delta
-
-# difftastic
-zinit ice lucid \
-  as'program' from'gh-r'
-zinit light Wilfred/difftastic
-
-# chezmoi
-zinit ice lucid \
-  as'program' from'gh-r' \
-  bpick'chezmoi_*.tar.gz' \
-  cp'completions/chezmoi.zsh -> _chezmoi'
-zinit light twpayne/chezmoi
+  bpick'*-x86_64-*' \
+  atload'eval "$(mcfly init zsh)"'
+zinit light cantino/mcfly
 
 # neovim
 # NOTE: GitHubのlatest releaseがバージョンタグではなく"stable"タグを指している。zinit updateができなくて困るのでバージョンタグを指定している。
@@ -82,15 +81,16 @@ zinit ice lucid \
   nocompletions
 zinit light neovim/neovim
 
-# ghq
-zinit ice lucid \
-  as'program' from'gh-r' \
-  pick'ghq_*/ghq'
-zinit light x-motemen/ghq
-
 # packer.nvim が存在しない場合はダウンロードする
 # 一度ダウンロードしたあとは packer.nvim 自身が管理してくれる
 if [ ! -d "$HOME/.local/share/nvim/site/pack/packer/start/packer.nvim" ]; then
   git clone --filter=blob:none https://github.com/wbthomason/packer.nvim.git \
     $HOME/.local/share/nvim/site/pack/packer/start/packer.nvim
 fi
+
+# onefetch
+zinit ice lucid \
+  as'program' from'gh-r' \
+  atclone'./onefetch --generate zsh > _onefetch' \
+  atpull"%atclone"
+zinit light o2sh/onefetch
