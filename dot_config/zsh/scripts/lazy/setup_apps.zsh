0fd5c5925d90e440bcce583c9a4ff70926004bbe
ciffelia <15273128+ciffelia@users.noreply.github.com>
2025-02-08 01:58:45 +0900

    load shell completion for uv and pdftrim

--- dot_config/zsh/scripts/lazy/setup_apps.zsh
+++ dot_config/zsh/scripts/lazy/setup_apps.zsh
@@ -8,6 +8,19 @@ if (( $+commands[kubectl] )); then
   source <(kubectl completion zsh)
 fi
 
+# uv
+if (( $+commands[uv] )); then
+  source <(uv generate-shell-completion zsh)
+fi
+if (( $+commands[uvx] )); then
+  source <(uvx --generate-shell-completion zsh)
+fi
+
+# pdftrim
+if (( $+commands[pdftrim] )); then
+  source <(pdftrim --generate-completion zsh)
+fi
+
 # bat
 zinit ice lucid \
   as'program' from'gh-r' \
