4e150a91b43cec53f931282f24e3b13905264d60
ciffelia <15273128+ciffelia@users.noreply.github.com>
2025-08-05 20:44:02 +0900

    refactor

--- dot_config/zsh/scripts/lazy/setup_completions.zsh
+++ dot_config/zsh/scripts/lazy/setup_completions.zsh
@@ -8,15 +8,15 @@ if (( $+commands[kubectl] )); then
   source <(kubectl completion zsh)
 fi
 
+# pdftrim
+if (( $+commands[pdftrim] )); then
+  source <(pdftrim --generate-completion zsh)
+fi
+
 # uv
 if (( $+commands[uv] )); then
   source <(uv generate-shell-completion zsh)
 fi
 if (( $+commands[uvx] )); then
   source <(uvx --generate-shell-completion zsh)
-fi
-
-# pdftrim
-if (( $+commands[pdftrim] )); then
-  source <(pdftrim --generate-completion zsh)
 fi
