6f7848b83f612664a23d09f372d0d0b82661af37
ciffelia <15273128+ciffelia@users.noreply.github.com>
2025-08-07 06:05:45 +0900

    update fish funtion description

--- dot_config/fish/functions/cd_to_ghq_repository.fish
+++ dot_config/fish/functions/cd_to_ghq_repository.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/env fish
 
-function cd_to_ghq_repository -d "Search ghq repositories with fzf and cd to selected one"
+function cd_to_ghq_repository -d "Search repositories managed by ghq and cd to selected one"
     set ghq_root (ghq root)
     set repo (ghq list | fzf --preview "onefetch --no-art -- '$ghq_root/{}'" --height 70%)
 
